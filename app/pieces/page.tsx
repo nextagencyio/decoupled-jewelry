9ca4a0c3c14b9a206a8fae1c2abfb53828d15aad
Jay Callicott <jrcallicott@gmail.com>
2026-03-08 08:42:46 -0500

    fix: mark header-dependent routes as dynamic

--- app/pieces/page.tsx
+++ app/pieces/page.tsx
@@ -7,6 +7,7 @@ import Header from '../components/Header'
 import PieceCard from '../components/PieceCard'
 
 export const revalidate = 3600
+export const dynamic = 'force-dynamic'
 
 export const metadata: Metadata = {
   title: 'Pieces | Lumina Fine Jewelry',
